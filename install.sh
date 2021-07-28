50519ec18a365dc7c7713b2914dbba25d347f8dd
0xK1r4 <87922923+0xK1r4@users.noreply.github.com>
2021-07-28 16:20:27 -0400

    Update install.sh
    instalar paquetes necesarios

--- install.sh
+++ install.sh
@@ -3,4 +3,10 @@ sudo apt-get install python 3
 pip3 install tabulate
 pip3 install colorama
 pip3 install psutil
-echo 'installed'
+echo 'creando alias...'
+mv slax /usr/local/bin
+echo 'creando permisos (775)'
+chmod 775 /usr/local/bin/slax 
+echo 'permisos agregador ..'
+echo 'slax instalado ! '
+echo 'para ejecutar escriba "slax"'
